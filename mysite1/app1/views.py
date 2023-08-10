fde7e6eeedd50022e3f44c6a0bc571f50ce9c845
Justas Mackevicius <1.justas.mackevicius@gmail.com>
2023-08-10 11:50:37 +0300

    register html, views updated to have registration

--- mysite1/app1/views.py
+++ mysite1/app1/views.py
@@ -1,6 +1,10 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import StockSymbol, StockPrediction
+from django.shortcuts import redirect
+from django.contrib.auth.forms import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -31,3 +35,32 @@ def homepage_view(request):
         'num_visits': num_visits,
     }
     return render(request, 'homepage.html', context=context)  # Render the homepage template
+
+@csrf_protect
+def register(request):
+    if request.method == "POST":
+        # Get values from the registration form
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # Check if passwords match
+        if password == password2:
+            # Check if the username is taken
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Username {username} is already taken!')
+                return redirect('register')
+            else:
+                # Check if the email is already registered
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, f'A user with the email {email} is already registered!')
+                    return redirect('register')
+                else:
+                    # If everything is okay, create a new user
+                    User.objects.create_user(username=username, email=email, password=password)
+                    messages.info(request, f'User {username} has been registered!')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('register')
+    return render(request, 'register.html')
