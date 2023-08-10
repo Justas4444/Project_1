8aa5c8418e869e89c25e3856c3bf3d03197d8dbd
Justas Mackevicius <1.justas.mackevicius@gmail.com>
2023-08-10 11:12:20 +0300

    password reset templates

--- mysite1/mysite1/settings.py
+++ mysite1/mysite1/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+# el. pašto adresas iš kurio siųsite
+EMAIL_HOST_PASSWORD = 'VerySecret'
+# slaptažodis
